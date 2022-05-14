65c123a296f9f214aec7307f650409edeaead81b
santibm20 <105526950+santibm20@users.noreply.github.com>
2022-05-13 20:10:38 -0500

    Create PanelLogo.java

---
+++ Triqui/UI/PanelLogo.java
@@ -0,0 +1,16 @@
+package com.usc.UI;
+
+import javax.swing.*;
+
+
+public class PanelLogo extends JPanel
+{
+    JLabel textoLogo;
+    
+    public PanelLogo()
+    {
+        textoLogo = new JLabel();
+        textoLogo.setText("aqui va el logo");
+        add(textoLogo);
+    }
+}
